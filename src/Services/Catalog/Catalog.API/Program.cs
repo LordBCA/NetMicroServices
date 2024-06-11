f0f0b11e4b14ca072b6bab7cde9fc261696ddb71
Diego Bocanegra <diegoa_boca@hotmail.com>
2024-06-11 17:36:27 -0500

    Catalog Api

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,25 +1,55 @@
+using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
+using Catalog.API.Data;
+using FluentValidation;
+using HealthChecks.UI.Client;
 using Marten;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
-//Carter
+// Carter
 builder.Services.AddCarter();
-//MediatR
+// MediatR
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
-//Marten
+// Fluent Validation
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+// Marten
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
+
+if (builder.Environment.IsDevelopment())
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
+
+//CustomExceptioHandler
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+//health check
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 //Carter
 app.MapCarter();
+
+//configure CustomException
+app.UseExceptionHandler(options => { });
+
+//configure Health check
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
